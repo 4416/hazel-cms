8af8f4a122d774c43fb752c0c68bf457f18944cb
Angermann Moritz <moritz.angermann@gmail.com>
2009-02-19 14:34:54 +0700

    implemented the cache(w)nut

---
+++ hazel/nuts/cache/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from wtforms import Form
+from wtforms import TextField
+from wtforms import SubmitField
+from wtforms.validators import regexp
+
+from hazel.models.settings import Settings
+
+from hazel import invalidate_urls
+from hazel.util.decorators import layout_global
+
+# there is nothing to configure.
+defaults = { } 
+
+# we could even overwrite this one,
+# FIXME: can we really or are we calling any DataStore related methods
+#        on the NutSettings object?
+NutSettings = lambda : Settings.get_or_create('settings:files', **defaults)
+
+import views
+
+__all__ = ['NutSettings']
--- hazel/nuts/cache/views.py
+++ hazel/nuts/cache/views.py
@@ -3,6 +3,9 @@ from google.appengine.api import memcache
 
 from hazel.util.helper import render_template
 
+from urls import expose_admin
+
+@expose_admin('/cache/', tab='Cache')
 def list(request):
     if request.args.get('flush',False):
         memcache.flush_all()
