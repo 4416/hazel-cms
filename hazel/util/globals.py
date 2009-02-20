19451d28c0beaebae4fd049046d563f0d4ed9bab
Angermann Moritz <moritz.angermann@gmail.com>
2009-02-20 16:38:26 +0700

    missing http as a valid url start

--- hazel/util/globals.py
+++ hazel/util/globals.py
@@ -5,6 +5,6 @@ from decorators import jinja_global
 @jinja_global
 def url_for(endpoint, _external=False, **values):
     url = local.adapter.build(endpoint, values, force_external=_external)
-    if url.startswith(u'/'):
+    if url.startswith(u'/') or url.startswith(u'http'):
         return url
     return u'/' + url
